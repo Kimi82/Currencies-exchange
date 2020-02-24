d4169ff260ab041a3d760cd1e8db40731c1a9e7f
Kimi82 <cyrekamil@gmail.com>
2020-02-24 23:23:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import date
 
+
 def checkConnection():
     response = requests.get("https://api.exchangeratesapi.io/")
     if response.status_code == 200:
@@ -10,43 +11,40 @@ def checkConnection():
     return status
 
 
-availableCurrencies="PLN ,CAD, HKD, ISK, PHP, DKK, HUF, CZK, GBP, RON, SEK, IDR, INR, BRL, RUB, HRK,\n JPY, THB, CHF, EUR, MYR, BGN, TRY, CNY, NOK, NZD, USD, MXN, SGD, AUD, ILS, KRW"
+availableCurrencies = "PLN ,CAD, HKD, ISK, PHP, DKK, HUF, CZK, GBP, RON, SEK, IDR, INR, BRL, RUB, HRK,\n JPY, THB, CHF, EUR, MYR, BGN, TRY, CNY, NOK, NZD, USD, MXN, SGD, AUD, ILS, KRW"
 baseCurrencies = input("Available currencies:\n %s.\nChoose your base: " % availableCurrencies)
 baseCurrencies = baseCurrencies.upper()
 currencies = input("Wybierz waluty do pokazania, wpisz je po spcji: ")
-currencies = currencies.upper().split(" ") #list
-def getCurrentCurrencies(baseCurrencies,currencies):
+currencies = currencies.upper().split(" ")  # list
 
 
+def getCurrentCurrencies(baseCurrencies, currencies):
     newCurrencies = None
-    if len(currencies)>1:
+    if len(currencies) > 1:
         newCurrencies = ','.join(currencies)
     else:
-        newCurrencies=''.join(currencies)
+        newCurrencies = ''.join(currencies)
 
-    baseCurrenciesResponse = requests.get('https://api.exchangeratesapi.io/latest?base='+baseCurrencies+'&symbols='+newCurrencies)
-
+    baseCurrenciesResponse = requests.get(
+        'https://api.exchangeratesapi.io/latest?base=' + baseCurrencies + '&symbols=' + newCurrencies)
 
     data = baseCurrenciesResponse.json()
-    #print(data)
+    # print(data)
     date = data['date']
     currenciesToArray = data['rates']
-    finalResult = {}
-
     keyArray = list(currenciesToArray.keys())
     currenciesArray = list(currenciesToArray.values())
-    i=0
-    while i>=i:
-        
-
+    finalResult = dict(zip(keyArray, currenciesArray))
+    finalResult.update(date = date)
+    print(finalResult)
     return currencies, baseCurrencies, baseCurrenciesResponse, date, finalResult
 
-def getHistoricalCurrencies(baseCurrencies,currencies):
+
+def getHistoricalCurrencies(baseCurrencies, currencies):
     begin = input("Enter the start date in USA notation(YYYY-MM-DD): ")
     end = input("Enter end date or don't write anythink to use today date: ")
-    if(end or len(end)<=1):
+    if (end or len(end) <= 1):
         end = date.today()
-
 
     newCurrencies = None
     if len(currencies) > 1:
@@ -54,10 +52,12 @@ def getHistoricalCurrencies(baseCurrencies,currencies):
     else:
         newCurrencies = ''.join(currencies)
 
-    baseCurrenciesResponse = requests.get('https://api.exchangeratesapi.io/history?start_at=' + begin +'&end_at=' + end + '&base=' + baseCurrencies + '&symbols=' + newCurrencies)
+    baseCurrenciesResponse = requests.get(
+        'https://api.exchangeratesapi.io/history?start_at=' + begin + '&end_at=' + end + '&base=' + baseCurrencies + '&symbols=' + newCurrencies)
     print(baseCurrenciesResponse.json())
 
-getCurrentCurrencies(baseCurrencies,currencies)
-#getHistoricalCurrencies(baseCurrencies,currencies)
+
+getCurrentCurrencies(baseCurrencies, currencies)
+# getHistoricalCurrencies(baseCurrencies,currencies)
 
 
